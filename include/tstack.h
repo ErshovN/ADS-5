55663756fe73e154ed4b3b4251b73f597b97acf8
ErshovN <99174712+ErshovN@users.noreply.github.com>
2022-04-03 22:58:14 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -9,23 +9,18 @@ class TStack {
     T arr[100];
     int top;
  public:
-    TStack():top(-1) { }
-    T get() const {
-        return arr[top];
-    }
-    bool isEmpty() const {
-        return top == -1;
-    }
-    bool isFull() const {
-        return top == size-1;
-    }
+    TStack(): top(-1) {}
+    bool isEmpty() { return top == -1; }
+    bool isFull() { return top == size-1; }
+    T get() { return arr[top]; }
     void pop() {
-        if (top >= 0)
-        top--;
+        if (!isEmpty()) --top;
     }
-    void push(T item) {
-        if (top < size-1)
-        arr[++top] = item;
+    void push(T value) {
+        if (isFull())
+           throw "full!";
+        else
+           arr[++top] = value;
     }
 };
 #endif  // INCLUDE_TSTACK_H_
