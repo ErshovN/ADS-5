815fd9be008f05cc85be809d07b25770e2f453ee
ErshovN <99174712+ErshovN@users.noreply.github.com>
2022-04-03 23:03:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,26 +1,96 @@
 // Copyright 2021 NNTU-CS
-#ifndef INCLUDE_TSTACK_H_
-#define INCLUDE_TSTACK_H_
+#include <string>
+#include <map>
+#include "tstack.h"
 
-template<typename T, int size>
-class TStack {
+int preference(char i) {
+    switch (i) {
+    case '(': return 0;
+    case ')': return 1;
+    case '-': return 2;
+    case '+': return 2;
+    case '*': return 3;
+    case '/': return 3;
+    case ' ': return 4;
+    default: return 4;
+    }
+}
 
- private:
-    T arr[100];
-    int top;
- public:
-    TStack(): top(-1) {}
-    bool isEmpty() { return top == -1; }
-    bool isFull() { return top == size-1; }
-    T get() { return arr[top]; }
-    void pop() {
-        if (!isEmpty()) --top;
+std::string infx2pstfx(std::string inf) {
+  // добавьте код
+  return std::string("");
+    TStack <char, 100> stack;
+    std::string tmp;
+    int pref;
+    for (int i = 0; i < inf.size(); i++) {
+        //std::cout << inf[i] - '0' << std::endl;
+        pref = preference(inf[i]);
+        if (pref == 4) {
+            tmp.push_back(inf[i]);
+            tmp.push_back(' ');
+        } else {
+            if (((pref == 0) || stack.isEmpty())) {
+                stack.push(inf[i]);
+            } else if ((pref > preference(stack.get()))) {
+                stack.push(inf[i]);
+            } else if (pref == 1) {
+                while (stack.get() != '(') {
+                    tmp.push_back(stack.get());
+                    tmp.push_back(' ');
+                    stack.pop();
+                }
+                stack.pop();
+            } else if (inf[i] != ' ') {
+                int j = preference(stack.get());
+                while ((j >= preference(inf[i])) && (!stack.isEmpty())) {
+                    tmp.push_back(stack.get());
+                    tmp.push_back(' ');
+                    stack.pop();
+                }
+                stack.push(inf[i]);
+            }
+        }
     }
-    void push(T value) {
-        if (isFull())
-           throw "full!";
-        else
-           arr[++top] = value;
+    while (!stack.isEmpty()) {
+        tmp.push_back(stack.get());
+        tmp.push_back(' ');
+        stack.pop();
     }
-};
-#endif  // INCLUDE_TSTACK_H_
+    if (tmp[tmp.size() - 1] == ' ') {
+        tmp.erase(tmp.size() - 1);
+    }
+    return tmp;}
+
+int calk(char sum, int x, int y) {
+    switch (sum)
+    {case '+': return x + y;
+    case '-': return x - y;
+    case '/':
+       if (y != 0) return x / y;
+    case '*': return x * y;
+    default:
+     return 0;
+        break;
+    }
+}
+
+
+int eval(std::string pref) {
+  // добавьте код
+  return 0;
+}
+    TStack <int, 100> stack;
+    int x, y;
+    for (int i = 0; i < pref.size(); i++) {
+        if (preference(pref[i]) < 4) {
+            y = stack.get();
+            stack.pop();
+            x = stack.get();
+            stack.pop();
+            stack.push(calk(pref[i], x, y));
+        } else if (preference(pref[i]) == 4 && pref[i] != ' ') {
+            stack.push(pref[i] - '0');
+        }
+    }
+    return stack.get(); 
+}
