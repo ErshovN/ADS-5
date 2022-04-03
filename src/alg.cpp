f8ad7516b9f7182ee4444205be236b3cdfcff965
ErshovN <99174712+ErshovN@users.noreply.github.com>
2022-04-03 22:29:43 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,109 @@
 #include <map>
 #include "tstack.h"
 
+int prior(char c) {
+    if (c == '(') {
+        return 0;
+    }
+    if (c == ')') {
+        return 1;
+    }
+    if (c == '+') {
+        return 2;
+    }
+    if (c == '-') {
+        return 2;
+    }
+    if (c == '*') {
+        return 3;
+    }
+    if (c == '/') {
+        return 3;
+    }
+    if (c == ' ') {
+        return 4;
+    } else {
+    return -1;
+    }
+}
+
 std::string infx2pstfx(std::string inf) {
   // добавьте код
   return std::string("");
+    std::string post;
+    TStack <char, 100> stack1;
+for (int i = 0; i < inf.size(); i++) {
+    if (prior(inf[i]) == -1) {
+        post.push_back(inf[i]);
+        post.push_back(' ');
+    } else {
+      if (prior(inf[i]) == 0) {
+        stack1.push(inf[i]);
+      } else if (stack1.isEmpty()) {
+        stack1.push(inf[i]);
+      } else if ((prior(inf[i]) > prior(stack1.get()))) {
+        stack1.push(inf[i]);
+      } else if (prior(inf[i]) == 1) {
+        while (prior(stack1.get()) != 0) {
+          post.push_back(stack1.get());
+          post.push_back(' ');
+          stack1.pop();
+        }
+            stack1.pop();
+        } else {
+            while (!stack1.isEmpty() && prior(inf[i]) <= prior(stack1.get())) {
+                post.push_back(stack1.get());
+                post.push_back(' ');
+                stack1.pop();
+            }
+            stack1.push(inf[i]);
+        }
+    }
+}
+while (!stack1.isEmpty()) {
+    post.push_back(stack1.get());
+    post.push_back(' ');
+    stack1.pop();
+}
+for (int i = 0; i < post.size(); i++) {
+    if (post[post.size() - 1] == ' ')
+      post.erase(post.size() - 1);
+  }
+  return post;
+}
+
+int calcul(char c, int b, int a) {
+    if ('+' == c) {
+        return a + b;
+    }
+    if ('-' == c) {
+        return a - b;
+    }
+    if ('*' == c) {
+        return a * b;
+    }
+    if ('/' == c && b != 0) {
+        return a / b;
+    } else {
+    return 0;
+    }
 }
 
 int eval(std::string pref) {
   // добавьте код
   return 0;
+TStack <int, 100> stack2;
+  for (int i = 0; i < pref.size(); i++) {
+    if (prior(pref[i]) == -1) {
+      stack2.push(pref[i] - '0');
+    } else if (prior(pref[i]) <= 3) {
+      int b = stack2.get();
+      stack2.pop();
+      int a = stack2.get();
+      stack2.pop();
+      stack2.push(calcul(pref[i], b, a));
+    }
+  }
+  int zn = stack2.get();
+  return zn;
 }
