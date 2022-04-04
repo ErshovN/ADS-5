05cf9bf7dd90bb9f378992edbe82a026e14c0aa2
ErshovN <99174712+ErshovN@users.noreply.github.com>
2022-04-04 14:29:24 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,7 +2,6 @@
 #include <string>
 #include <map>
 #include "tstack.h"
-
 int priority(char c) {
   switch (c) {
     case '(': return 0;
@@ -14,7 +13,6 @@ int priority(char c) {
   }
   return -1;
 }
-
 bool isDigit(std::string pref) {
   for (size_t i = 0; i < pref.size(); ++i) {
     if (pref[i] < '0' || pref[i] > '9')
@@ -29,37 +27,7 @@ std::string infx2pstfx(std::string inf) {
   TStack <char, 50> stack;
   std::string result;
   for (int i = 0; i < inf.size(); i++) {
-    int p = priority(inf[i]);
-    if (p == -1) {
-      if (!result.empty() && priority(inf[i - 1]) != -1) {
-        result.push_back(' ');
-      }
-      result.push_back(inf[i]);
-    } else if (p == 0 || p > priority(stack.get()) || stack.isEmpty()) {
-      stack.push(inf[i]);
-    } else {
-      if (p == 1) {
-        while (stack.get() != '(') {
-          result.push_back(' ');
-          result.push_back(stack.get());
-          stack.pop();
-        }
-        stack.pop();
-      } else {
-        while (priority(stack.get()) >= p) {
-          result.push_back(' ');
-          result.push_back(stack.get());
-          stack.pop();
-        }
-        stack.push(inf[i]);
-      }
-    }
-  }
-  while (!stack.isEmpty()) {
-    result.push_back(' ');
-    result.push_back(stack.get());
-    stack.pop();
-  }
+@@ -63,9 +61,6 @@ std::string infx2pstfx(std::string inf) {
   return result;
 }
 
